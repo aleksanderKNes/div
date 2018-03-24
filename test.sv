b58435d59340b394d2e72d9390fc76bde2f3d58a
Aleksander Nes <aleksander.nes@gmail.com>
2018-03-24 22:04:03 +0100

    Local commit after squashing

--- test.sv
+++ test.sv
@@ -8,9 +8,9 @@ created file
 Change on master on github
 
 
+  This is a change I make on the local master branch, but I want to move it to the remote branch.
 
-
-
+    also want to move this to the branch 
 
 
 
